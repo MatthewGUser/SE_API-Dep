27d9a0af6d1f4fabbb97b451517576673c377773
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-02-05 19:17:17 -0500

    update

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,15 @@
 from app import create_app
 from app.models import db
-from config import config
+from app.config import config
 
 app = create_app(config['production'])
 
-# Update Swagger config
 app.config.update({
     'SWAGGER': {
         'title': 'Your API',
         'uiversion': 3,
         'specs_route': '/swagger/',
-        'host': 'se-api-dep.onrender.com',  # Remove https:// prefix
+        'host': 'your-app-name.onrender.com',
         'schemes': ['https']
     }
 })
